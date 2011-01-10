e47def720c15513dc3f3ceebc804d8c8ca12e730
Yssaril <N/A>
2011-01-09 18:15:04 -0600

    did some more testing nd finaly figured it out enjoy

--- Core.lua
+++ Core.lua
@@ -1,6 +1,6 @@
 local _, AEQI = ...
 
-_G['AEQI'] = AEQI
+--_G['AEQI'] = AEQI --debug code
 AEQI.eventframe = CreateFrame('frame')
 AEQI.eventframe:SetScript("OnEvent", function(self, event, ...) if AEQI[event] then return AEQI[event](AEQI, event, ...) end end)
 function AEQI:RegisterEvent(...) return AEQI.eventframe:RegisterEvent(...) end
@@ -8,7 +8,7 @@ function AEQI:UnregisterEvent(...) return AEQI.eventframe:UnregisterEvent(...) e
 
 AEQI:RegisterEvent("QUEST_COMPLETE")
 
-local itemequip = ""
+AEQI.itemequip = ""
 
 local button = AEQI:GetWidget("button", QuestFrameRewardPanel)
 	button:SetPoint("LEFT", QuestFrameCompleteQuestButton, "RIGHT", 5, 0)
@@ -16,14 +16,13 @@ local button = AEQI:GetWidget("button", QuestFrameRewardPanel)
 	button:SetWidth(200)
 	button:SetText("Complete Quest & Equip Item")
 	button:SetScript("OnClick", function(...)
-		itemequip = GetQuestItemLink("choice", QuestInfoFrame.itemChoice)
+		AEQI.itemequip = tonumber(GetQuestItemLink("choice", QuestInfoFrame.itemChoice):match("item:(%d+)"))
 		QuestRewardCompleteButton_OnClick()
 		AEQI:RegisterEvent("BAG_UPDATE")
 	end)
 	
 function AEQI:QUEST_COMPLETE()
 	button:Hide()
-	--print("button Hide")
 end
 
 function AEQI:BestSlot(bagID,slot)
@@ -43,39 +42,40 @@ function AEQI:BestSlot(bagID,slot)
 end
 
 function AEQI:BAG_UPDATE()
-	local equiped
-	for bagID=0, NUM_BAG_SLOTS do
-		for slot=1, GetContainerNumSlots(bagID) do
-			if itemequip == GetContainerItemLink(bagID, slot) then
-				local invSlot = self:BestSlot(bagID,slot)
-				PickupContainerItem(bagID, slot)
-				if not InCombatLockdown then
-					EquipCursorItem(invSlot)
-					itemequip = ""
-				else
-					self:RegisterEvent("PLAYER_REGEN_ENABLED")
+	if not InCombatLockdown() then
+		local equiped
+		for bagID=0, NUM_BAG_SLOTS do
+			for slot=1, GetContainerNumSlots(bagID) do
+				local link = GetContainerItemLink(bagID, slot) or ""
+				if link and (AEQI.itemequip == tonumber(link:match("item:(%d+)"))) then
+					local invSlot = self:BestSlot(bagID,slot)
+					PickupContainerItem(bagID, slot)
+					PickupInventoryItem(invSlot)
+					AEQI.itemequip = ""
+					self:UnregisterEvent("BAG_UPDATE")
+					break
 				end
-				self:UnregisterEvent("BAG_UPDATE")
+			end
+			if equiped then
 				break
 			end
 		end
-		if equiped then
-			break
-		end
+		ClearCursor()
+	else
+		self:RegisterEvent("PLAYER_REGEN_ENABLED")
 	end
-	ClearCursor()
 end
 
 function AEQI:PLAYER_REGEN_ENABLED()
 	local equiped
 	for bagID=0, NUM_BAG_SLOTS do
 		for slot=1, GetContainerNumSlots(bagID) do
-			if itemequip == GetContainerItemLink(bagID, slot) then
+			if itemequip == GetContainerItemLink(bagID, slot):match("item:(%d+)") then
 				local invSlot = self:BestSlot(bagID,slot)
 				PickupContainerItem(bagID, slot)
-				EquipCursorItem(invSlot)
+				PickupInventoryItem(invSlot)
 				itemequip = ""
-				self:UnregisterEvent("PLAYER_REGEN_ENABLED")
+				self:UnregisterEvent("BAG_UPDATE") -- just in case a borderline case happens when combat ends right as the bags update
 				equiped = true
 				break
 			end
@@ -85,6 +85,7 @@ function AEQI:PLAYER_REGEN_ENABLED()
 		end
 	end
 	ClearCursor()
+	self:UnregisterEvent("PLAYER_REGEN_ENABLED") -- only run this once
 end
 
 local QuestInfoItem_OnClick_old = QuestInfoItem_OnClick
@@ -98,4 +99,3 @@ function QuestInfoItem_OnClick(self, ...)
 		end
 	end
 end
-
